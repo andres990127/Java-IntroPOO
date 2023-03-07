d548ccddf4d4feb2c2480dfc196a2abbed8c2db9
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-03-07 16:29:23 -0500

    Creando la clase Patient

--- src/Doctor.java
+++ src/Doctor.java
@@ -2,12 +2,18 @@ public class Doctor {
     //Atributes
     static int id;
     String name;
+    String email;
     String speciality;
 
     //Constructor
     Doctor(){
         System.out.println("Se ha creado un nuevo Doctor");
         id++;
+    }
+
+    public Doctor(String name, String speciality) {
+        this.name = name;
+        this.speciality = speciality;
     }
 
     //Métodos

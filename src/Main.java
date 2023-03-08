0abfe22327fb7034adfcfed64aca6bf46ece10f4
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-03-08 17:12:20 -0500

    Implementando sobreescritura de métodos

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,26 @@ import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
+
         //showMenu();
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Andrés Gutiérrez", "andres@mail.com","Pediatria");
         myDoctor.addAvailableAppointment(new Date(), "4pm");
         myDoctor.addAvailableAppointment(new Date(), "10am");
-        myDoctor.addAvailableAppointment(new Date(), "3pm");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        for(Doctor.AvailableAppointment availableAppointment: myDoctor.getAvailableAppointments()){
-            System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
         }
+
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        System.out.println(patient);
+
     }
+
+
+
 }
